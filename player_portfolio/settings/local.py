2b3fffd57b74e4c03f4bd8d549f20cea2c29c0b5
Josh Isaacson <jsisaacs@iu.edu>
2018-12-07 12:21:25 -0500

    added .herokudns.com to ALLOWED_HOSTS

--- player_portfolio/settings/local.py
+++ player_portfolio/settings/local.py
@@ -26,7 +26,7 @@ SECRET_KEY = '-qyh!-vle!qzb^*q$pvnpqeelqjdo*g$*64^x5ops4-@w2#=_('
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '.herokuapp.com', '0.0.0.0', 'localhost', '127.0.0.1', '.player-portfolio.com'
+    '.herokuapp.com', '0.0.0.0', 'localhost', '127.0.0.1', '.player-portfolio.com', '.herokudns.com'
 ]
 
 
