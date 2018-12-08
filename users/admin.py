cd8789533408b570751306d27ecce864c5cdee7c
Josh Isaacson <jsisaacs@iu.edu>
2018-12-08 12:50:29 -0500

    finished general stylings of landing page

--- users/admin.py
+++ users/admin.py
@@ -11,8 +11,4 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ['first_name', 'last_name', 'email', 'username', 'date_joined']
 
-    # fieldsets = UserAdmin.fieldsets + (
-    #         (None, {'fields': ('first',)}),
-    # )
-
 admin.site.register(CustomUser, CustomUserAdmin)
